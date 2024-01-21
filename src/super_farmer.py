59acf017105515259f25ede1021484c56b8adada
Dawid Chmielewski <dawid.chmielewski@abax.com>
2024-01-21 22:30:06 +0100

    Super farmer - ✨Adding animals value, user animals, and exchange prototype.

--- src/super_farmer.py
+++ src/super_farmer.py
@@ -1,3 +1,4 @@
+import math
 import random
 from enum import Enum
 
@@ -7,10 +8,22 @@ class Animal(Enum):
     SHEEP = 'Sheep'
     PIG = 'Pig'
     COW = 'Cow'
-    HORSE = 'Hors'
+    HORSE = 'Horse'
     FOX = 'Fox'
     WOLF = 'Wolf'
+    SMALL_DOG = 'Small dog'
+    BIG_DOG = 'Big dog'
 
+
+animal_value = {
+    Animal.RABBIT: 1,
+    Animal.SHEEP: 6,
+    Animal.SMALL_DOG: 6,
+    Animal.PIG: 12,
+    Animal.COW: 36,
+    Animal.HORSE: 72,
+    Animal.BIG_DOG: 36,
+}
 
 blue_dice = 1 * [Animal.COW] + \
             1 * [Animal.WOLF] + \
@@ -24,6 +37,41 @@ orange_dice = 1 * [Animal.HORSE] + \
               2 * [Animal.PIG] + \
               6 * [Animal.RABBIT]
 
+user_animals = []
+
+
+def has_enough_animals(animal, quantity):
+    return user_animals.count(animal) >= quantity
+
+
+def remove_animals(animal, quantity):
+    for _ in range(quantity):
+        user_animals.remove(animal)
+
+
+def add_animals(animal, quantity):
+    user_animals.extend([animal] * quantity)
+
+
+def calculate_exchange_quantity(animal_to_give, give_quantity, animal_to_receive):
+    receive_ = animal_value[animal_to_give] * give_quantity / animal_value[animal_to_receive]
+    print(f"RECEIVE: {receive_}")
+    return math.floor(receive_)
+
+
+def exchange_animals(animal_to_give, give_quantity, animal_to_receive):
+    print(f"Trying to give {give_quantity} {animal_to_give.value} to get {animal_to_receive.value}.")
+    required_quantity = calculate_exchange_quantity(animal_to_give, give_quantity, animal_to_receive)
+    if required_quantity is not None and required_quantity >= 1:
+        # if has_enough_animals(animal_to_give, give_quantity):
+        print(f"Exchanging {give_quantity} {animal_to_give.value} to {required_quantity} {animal_to_receive.value}.")
+        # remove_animals(animal_to_give, give_quantity)
+        # add_animals(animal_to_receive, required_quantity)
+    # else:
+    #     print(f"Not enough {animal_to_give.value} to exchange.")
+    else:
+        print(f"Cannot exchange {give_quantity} {animal_to_give.value} for {animal_to_receive.value}.")
+
 
 def roll_both_dice():
     blue_result = random.choice(blue_dice)
@@ -31,4 +79,9 @@ def roll_both_dice():
     return blue_result, orange_result
 
 
-print(roll_both_dice())
+print("Roll | Blue / Orange")
+for i in range(100):
+    blue_roll, orange_roll = roll_both_dice()
+    print(f"{i + 1:4} | {blue_roll.value}/{orange_roll.value}")
+
+print(exchange_animals(Animal.PIG, 5, Animal.HORSE))
